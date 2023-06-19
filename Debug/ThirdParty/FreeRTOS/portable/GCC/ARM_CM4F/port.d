dc52c26765d52514fb8b41757145e4408d94b95b
pattnasa2013 <sagarpattnaik60@gmail.com>
2023-06-18 21:38:10 -0400

    Integrated FreeRTOS to STM32Discovery
    Initial Commit For FreeRTOS project

---
+++ Debug/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.d
@@ -0,0 +1,20 @@
+ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.o: \
+ ../ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/port.c \
+ C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/task.h \
+ C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/list.h
+C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/portable.h:
+C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/task.h:
+C:/Sagar/Repo/GitHub/Demo/FreeRtos_Discovery/Project/ThirdParty/FreeRTOS/include/list.h:
