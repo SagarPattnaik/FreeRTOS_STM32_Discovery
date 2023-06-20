5ddcd4296101c1833be2e48e7b60fcd5efddc5c6
pattnasa2013 <sagarpattnaik60@gmail.com>
2023-06-19 23:34:02 -0400

    SingleShotRecording

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -44,7 +44,7 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
+#define DWT_CTRL    (*(volatile uint32_t*)0xE0001000)
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -91,7 +91,10 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-
+  //Enable the CYCCNT counter.
+  DWT_CTRL |= ( 1 << 0);
+  SEGGER_SYSVIEW_Conf();
+  SEGGER_SYSVIEW_Start();
   status = xTaskCreate(task1_handler, "Task-1", 200, "Hello world from Task-1", 2, &task1_handle);
 
   configASSERT(status == pdPASS); /* Trap to an Infinite Loop here if status Fails */
@@ -310,7 +313,7 @@ static void task1_handler(void* parameters)
 	while(1)
 	{
     printf("%s\n", (char*)parameters);
-    taskYIELD(); /* Force a context switch */
+    /* taskYIELD(); */ /* Force a context switch */
   }
 }
 static void task2_handler(void* parameters)
@@ -318,7 +321,7 @@ static void task2_handler(void* parameters)
 	while(1)
 	{
     printf("%s\n", (char*)parameters);
-    taskYIELD(); /* Force a context switch */    
+    /* taskYIELD(); */ /* Force a context switch */    
   }
 }
 /* USER CODE END 4 */
