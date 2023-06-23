1c702b36f8e3eca74b416aaa10d8255f8c62c480
pattnasa2013 <sagarpattnaik60@gmail.com>
2023-06-23 15:55:56 -0400

    SySView_Continuous_UART_Based_Recording

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -53,6 +53,7 @@ static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 static void task1_handler(void* parameters);
 static void task2_handler(void* parameters);
+extern  void SEGGER_UART_init(uint32_t);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -93,8 +94,9 @@ int main(void)
   /* USER CODE BEGIN 2 */
   //Enable the CYCCNT counter.
   DWT_CTRL |= ( 1 << 0);
+  SEGGER_UART_init(250000);
   SEGGER_SYSVIEW_Conf();
-  SEGGER_SYSVIEW_Start();
+  /* SEGGER_SYSVIEW_Start(); */
   status = xTaskCreate(task1_handler, "Task-1", 200, "Hello world from Task-1", 2, &task1_handle);
 
   configASSERT(status == pdPASS); /* Trap to an Infinite Loop here if status Fails */
@@ -142,8 +144,8 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 50;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -159,7 +161,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
   {
     Error_Handler();
   }
@@ -310,18 +312,24 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 static void task1_handler(void* parameters)
 {
+	char msg[100];
 	while(1)
 	{
-    printf("%s\n", (char*)parameters);
-    /* taskYIELD(); */ /* Force a context switch */
+	  /*   printf("%s\n", (char*)parameters); */ /* For SWV */
+		snprintf(msg,100,"%s\n", (char*)parameters);
+		SEGGER_SYSVIEW_PrintfTarget(msg);
+		taskYIELD();
   }
 }
 static void task2_handler(void* parameters)
 {
+	char msg[100];
 	while(1)
 	{
-    printf("%s\n", (char*)parameters);
-    /* taskYIELD(); */ /* Force a context switch */    
+	  /*   printf("%s\n", (char*)parameters); */ /* For SWV */
+		snprintf(msg,100,"%s\n", (char*)parameters);
+		SEGGER_SYSVIEW_PrintfTarget(msg);
+		taskYIELD();
   }
 }
 /* USER CODE END 4 */
